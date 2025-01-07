a361d06d7e0aec54ccebf29bd5293eebea2c38d2
Msizi Mkhize <msizimkhize.rsa@gmail.com>
2025-01-07 04:41:09 -0500

    Delete Dataset/data_integrity_check.py

--- Dataset/data_integrity_check.py
+++
@@ -1,9 +0,0 @@
-# we first import needful packages
-import numpy as np # data arrays
-import pandas as pd # data structure and data analysis
-
-# we read the CSV file
-crimes = pd.read_csv("../Dataset/crime_incidents_by_category.csv")
-
-# we get the sum of missing data points per column
-missing_values_count = crimes.isnull().sum()
